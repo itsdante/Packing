6674201f22884693989ff6b007e865ae1d3e9d69
richardreynaldo <richardreynaldo626@gmail.com>
2020-08-05 00:12:17 +0700

    API sukses

--- Packing/Packing/API/ApiManager.swift
+++ Packing/Packing/API/ApiManager.swift
@@ -2,10 +2,34 @@
 import SwiftyJSON
 
 class APIManager {
-let baseurl = "http://157.230.252.80:5050/api/v1" // web gw
+let baseurl = "http://128.199.215.67:3000/api/v1" // web gw
 static let sharedInstance = APIManager() //api managernya
 static let getPostsEndpoint = "/recipes" //endpoint belakang tiap orang beda
 
+    func getActivities(gender: String, onSuccess: @escaping(JSON) -> Void, onFailure: @escaping(Error) -> Void){
+        let url : String = baseurl + "/activities?filter[where][gender]=\(gender)"
+        let request : NSMutableURLRequest = NSMutableURLRequest(url: NSURL(string: url)! as URL)
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {data, response, error -> Void in
+            if(error != nil){
+                onFailure(error!)
+            }else {
+                do {
+                let result = try JSON(data: data!)
+                    print(result)
+                    onSuccess(result)
+                } catch {
+                    //error
+                }
+                
+                
+            }
+        })
+        task.resume()
+    }
+    
 //contoh function
 func getWeather(recipeId: Int, onSuccess: @escaping(JSON) -> Void, onFailure: @escaping(Error) -> Void){
     let url : String = baseurl + APIManager.getPostsEndpoint + String(recipeId)
@@ -29,6 +53,8 @@ func getWeather(recipeId: Int, onSuccess: @escaping(JSON) -> Void, onFailure: @e
     })
     task.resume()
 }
+    
+    
 func getBooking(onSuccess: @escaping(JSON) -> Void, onFailure: @escaping(Error) -> Void){
        let url : String = baseurl + APIManager.getPostsEndpoint
        let request : NSMutableURLRequest = NSMutableURLRequest(url: NSURL(string: url)! as URL)
--- Packing/Packing/Views/NewTrip/SelectGenderPage.swift
+++ Packing/Packing/Views/NewTrip/SelectGenderPage.swift
@@ -43,6 +43,29 @@ struct SelectGenderPage: View {
                 
                 Spacer()
                     .frame(width: UIScreen.main.bounds.width * 0.05)
+                Button(action: {
+                    
+                    APIManager.sharedInstance.getActivities(
+                        gender: "female",
+                        onSuccess: {
+                            json in DispatchQueue.main.async {
+                                for i in 0 ..< json.count {
+                                    let newData = json[i];
+                                    print(json)
+//                                    let url = URL(string: newData[i])
+                                }
+                            }
+                    },
+                        onFailure: {
+                            error in
+                            print(error.localizedDescription)
+                    })
+                    
+                    
+                    
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+                }
                 
                 Button(action: {
                     self.selectedGender = "Female"
