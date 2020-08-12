d0c6fb691dcb38682b05d2c071be34921a40f0ca
richardreynaldo <richardreynaldo626@gmail.com>
2020-08-12 17:58:07 +0700

    Failed to found booking code

--- Packing/Packing/Models/Entities/Trip.swift
+++ Packing/Packing/Models/Entities/Trip.swift
@@ -47,5 +47,7 @@ let tripHongKong = Trip(bookingNumber: "71938JC", airline: "Lion Air", flightNum
 
 let tripAmsterdam = Trip(bookingNumber: "60827IB", airline: "Turkish Airlines", flightNumber: "FZ3717", originAirportCode: "CGK", originAirport: "Soekarno-Hatta Int. Airport", originCity: "Jakarta", originCountry: "Indonesia", destinationAirportCode: "AMS", destinationAirport: "Amsterdam Airport Schiphol", destinationCity: "Amsterdam", destinationCountry: "Netherlands", departureDate: Calendar.current.date(byAdding: .day, value: 7, to: Date())!, arrivalDate: Calendar.current.date(byAdding: .day, value: 10, to: Date())!.addingTimeInterval(5000), createdAt: Date(), luggages: [], restrictions: restrictionArray)
 
+let tripAmerica = Trip(bookingNumber: "28327JT", airline: "All Nippon Airways", flightNumber: "PN3827", originAirportCode: "CGK", originAirport: "Soekarno-Hatta Int. Airport", originCity: "Jakarta", originCountry: "Indonesia", destinationAirportCode: "NRT", destinationAirport: "Tokyo Narita Airport", destinationCity: "Tokyo", destinationCountry: "Japan", departureDate: Calendar.current.date(byAdding: .day, value: 7, to: Date())!, arrivalDate: Calendar.current.date(byAdding: .day, value: 7, to: Date())!.addingTimeInterval(5000), createdAt: Date(), luggages: [], restrictions: restrictionArray)
+
 var tripArray: [Trip] = []
 #endif
--- Packing/Packing/Views/Home/HomeInitialCard.swift
+++ Packing/Packing/Views/Home/HomeInitialCard.swift
@@ -27,7 +27,39 @@ struct HomeInitialCard: View {
     @ObservedObject private var keyboard = KeyboardResponder()
     @State private var newbookingNumber = ""
     
-    var trip = Trip(bookingNumber: "71938JC", airline: "Lion Air", flightNumber: "GA4828", originAirportCode: "CGK", originAirport: "Soekarno-Hatta Int. Airport", originCity: "Jakarta", originCountry: "Indonesia", destinationAirportCode: "HKG", destinationAirport: "Hong Kong Int. Airport", destinationCity: "Hong Kong", destinationCountry: "China", departureDate: Calendar.current.date(byAdding: .hour, value: 24, to: Date())!, arrivalDate: Calendar.current.date(byAdding: .day, value: 4, to: Date())!.addingTimeInterval(5000), createdAt: Date(), luggages: [], restrictions: [])
+
+    let BookCode = ["60827IB","71938JC","82049KD","28327JT"]
+
+    func checkTripReal(x: String) -> Bool {
+        if BookCode.contains(x)
+        {
+            return false
+        }
+        return true
+    }
+    
+    @State var isRealTrip: Bool = false
+
+    func checkTrip(x: String) -> Trip {
+        if x == "71938JC" {
+          return Trip(bookingNumber: "71938JC", airline: "Lion Air", flightNumber: "GA4828", originAirportCode: "CGK", originAirport: "Soekarno-Hatta Int. Airport", originCity: "Jakarta", originCountry: "Indonesia", destinationAirportCode: "HKG", destinationAirport: "Hong Kong Int. Airport", destinationCity: "Hong Kong", destinationCountry: "China", departureDate: Calendar.current.date(byAdding: .hour, value: 24, to: Date())!, arrivalDate: Calendar.current.date(byAdding: .day, value: 4, to: Date())!.addingTimeInterval(5000), createdAt: Date(), luggages: [], restrictions: [])
+        }
+        else if x == "28327JT"
+        {
+            return Trip(bookingNumber: "28327JT", airline: "All Nippon Airways", flightNumber: "PN3827", originAirportCode: "CGK", originAirport: "Soekarno-Hatta Int. Airport", originCity: "Jakarta", originCountry: "Indonesia", destinationAirportCode: "NRT", destinationAirport: "Tokyo Narita Airport", destinationCity: "Tokyo", destinationCountry: "Japan", departureDate: Calendar.current.date(byAdding: .day, value: 7, to: Date())!, arrivalDate: Calendar.current.date(byAdding: .day, value: 7, to: Date())!.addingTimeInterval(5000), createdAt: Date(), luggages: [], restrictions: restrictionArray)
+        }
+        else if x == "60827IB"
+        {
+            return Trip(bookingNumber: "60827IB", airline: "Turkish Airlines", flightNumber: "FZ3717", originAirportCode: "CGK", originAirport: "Soekarno-Hatta Int. Airport", originCity: "Jakarta", originCountry: "Indonesia", destinationAirportCode: "AMS", destinationAirport: "Amsterdam Airport Schiphol", destinationCity: "Amsterdam", destinationCountry: "Netherlands", departureDate: Calendar.current.date(byAdding: .day, value: 7, to: Date())!, arrivalDate: Calendar.current.date(byAdding: .day, value: 10, to: Date())!.addingTimeInterval(5000), createdAt: Date(), luggages: [], restrictions: restrictionArray)
+        }
+        else if x == "82049KD"
+        {
+           return Trip(bookingNumber: "82049KD", airline: "Emirates", flightNumber: "HB5939", originAirportCode: "CGK", originAirport: "Soekarno-Hatta Int. Airport", originCity: "Jakarta", originCountry: "Indonesia", destinationAirportCode: "LHR", destinationAirport: "London Heathrow Airport", destinationCity: "London", destinationCountry: "United Kingdom", departureDate: Calendar.current.date(byAdding: .day, value: -7, to: Date())!, arrivalDate: Calendar.current.date(byAdding: .day, value: -5, to: Date())!.addingTimeInterval(5000), createdAt: Date(), luggages: luggageArray, restrictions: restrictionArray)
+        }
+        return Trip(bookingNumber: "28327JT", airline: "All Nippon Airways", flightNumber: "PN3827", originAirportCode: "CGK", originAirport: "Soekarno-Hatta Int. Airport", originCity: "Jakarta", originCountry: "Indonesia", destinationAirportCode: "NRT", destinationAirport: "Tokyo Narita Airport", destinationCity: "Tokyo", destinationCountry: "Japan", departureDate: Calendar.current.date(byAdding: .day, value: 7, to: Date())!, arrivalDate: Calendar.current.date(byAdding: .day, value: 7, to: Date())!.addingTimeInterval(5000), createdAt: Date(), luggages: [], restrictions: restrictionArray)
+    }
+    
+    
     
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -60,7 +92,8 @@ struct HomeInitialCard: View {
                                 .multilineTextAlignment(TextAlignment.center)
                                 .padding(10)
                     ).frame(width: UIScreen.main.bounds.width * 0.53, height: UIScreen.main.bounds.height * 0.04)
-                    NavigationLink(destination: FlightInfoPage(trip: trip), isActive: $isRootActive) {
+                    
+                    NavigationLink(destination: FlightInfoPage(trip: checkTrip(x: self.newbookingNumber)), isActive: $isRootActive) {
                         RoundedRectangle(cornerRadius: 18)
                             .shadow(color: HomeInitialCard.buttonShadowColor, radius: 4, y: 5)
                             .overlay(
@@ -68,14 +101,24 @@ struct HomeInitialCard: View {
                                     .font(HomeInitialCard.poppinsSemiBold12)
                                     .foregroundColor(.white)
                         )
-                    }
+                        }
+                        
                     .isDetailLink(false)
                     .frame(width: UIScreen.main.bounds.width * 0.4, height: UIScreen.main.bounds.height * 0.04)
                     .foregroundColor(HomeInitialCard.buttonColor)
                     .padding(.top, 20)
+                    .disabled(checkTripReal(x: self.newbookingNumber))
+                    .simultaneousGesture(TapGesture().onEnded{
+                        
+                        if self.checkTripReal(x: self.newbookingNumber)
+                        {
+                            self.isRealTrip.toggle()
+                        }
+                    })
                 }
             }
             .frame(height: UIScreen.main.bounds.width * 0.45)
+            
         }
         .frame(width: UIScreen.main.bounds.width * 0.8, height: UIScreen.main.bounds.height * 0.66)
         .cornerRadius(40)
@@ -87,6 +130,9 @@ struct HomeInitialCard: View {
                 self.isRootActive = false
                 self.appState.moveToRoot = false
             }
+        }.alert(isPresented: self.$isRealTrip)
+        {
+            Alert(title: Text("Flight Not Found"), message: Text("The booking code that you input is not valid"), dismissButton: nil)
         }
     }
 }
